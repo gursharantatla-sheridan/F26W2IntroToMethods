2ae522e328a5ea5adb93780d470b36cc16deb633
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-15 09:17:42 -0400

    Add project files.

---
+++ F26W2IntroToMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace F26W2IntroToMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
