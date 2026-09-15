e550661224571d258a5a20482ce0d8d30d766826
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-15 10:24:26 -0400

    output params

--- F26W2IntroToMethods/Program.cs
+++ F26W2IntroToMethods/Program.cs
@@ -16,16 +16,47 @@
             Console.WriteLine("c.radius = " + c.radius);
             Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValues(n, s, c, a);
+            ChangeValues(ref n, ref s, c, a);
 
             Console.WriteLine("\nAfter changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("c.radius = " + c.radius);
             Console.WriteLine("a[0] = " + a[0]);
+
+            Console.WriteLine("\n\n");
+
+
+            // output parameters example
+            int r = 5;
+            //double circum, area;
+
+            GetCircumAndArea(r, out double circum, out double area);
+
+            Console.WriteLine("Circumference = " + circum.ToString("F"));
+            Console.WriteLine("Area = " + area.ToString("F"));
+
+
+
+
+            Console.Write("\n\nEnter a number: ");
+            
+            if (int.TryParse(Console.ReadLine(), out int num))
+                Console.WriteLine("\nnum = " + num);
+            else
+                Console.WriteLine("Invalid input. Please try again");
         }
 
-        static void ChangeValues(int num, string str, Circle cir, int[] arr)
+
+        static void GetCircumAndArea(int r, out double circum, out double area)
+        {
+            circum = 2 * Math.PI * r;
+            area = Math.PI * r * r;
+        }
+
+
+
+        static void ChangeValues(ref int num, ref string str, Circle cir, int[] arr)
         {
             num = num + 100;
             str = "bye";
