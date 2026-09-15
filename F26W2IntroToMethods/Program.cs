8f2342a944bc3873853ae90b616a7a0759976894
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-15 09:25:31 -0400

    value params

--- F26W2IntroToMethods/Program.cs
+++ F26W2IntroToMethods/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 5;
+
+            Console.WriteLine("Before changes:");
+            Console.WriteLine("n = " + n);
+
+            ChangeValues(n);
+
+            Console.WriteLine("\nAfter changes:");
+            Console.WriteLine("n = " + n);
+        }
+
+        static void ChangeValues(int num)
+        {
+            num = num + 100;
         }
     }
 }
